1cd8dce3650b1466e63aa3d338bb0eb456e21c4e
DPotton <130091579+DPotton@users.noreply.github.com>
2024-04-24 09:41:03 -0500

    Added functionality for the user to return to home/make a new post after entering the pokemon page.

--- pokemon.php
+++ pokemon.php
@@ -1,8 +1,13 @@
+<nav class="navigationSection"> 
+    <h2><a id="title" href="index.php">Dan's Pokemon Blog</a></h2>
+    <ul class="container">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="post.php" class="button-primary-outline">New Post!</a></li>
+    </ul>
+</nav>
 <?php
 
 require('connect.php');
-
-
 
 try {
     // Step 2: Retrieve data from the "pokemon" table
@@ -24,4 +29,6 @@ try {
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
+
+
 ?>
